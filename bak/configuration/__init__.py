e1f3c67f4d9e0e43e43595ef23bb7716f567c16d
ChanceNCounter <ChanceNCounter@icloud.com>
2021-01-30 15:06:27 -0800

    add `bak config`, fastmode, improve spaghetti

--- bak/configuration/__init__.py
+++ bak/configuration/__init__.py
@@ -13,15 +13,28 @@ class BakConfiguration(dict):
         'bak_database_location': 'null',
         'bak_diff_exec': 'null',
         'bak_list_relative_paths': 'false',
-        'bak_list_colors': 'true'
+        'bak_list_colors': 'true',
+        'fast_mode': 'false'
+    }
+
+    SETTABLE_VALUES = {
+        'relative-paths': 'bak_list_relative_paths',
+        'colors': 'bak_list_colors',
+        'fast-mode': 'fast_mode'
     }
     cfg: Config
     config_file: str
     data_dir: Path
     config_dir: Path
+    newline: str
 
+    values: dict
     def __init__(self):
-# Establish config file and database locations
+
+        self.values = {}
+        self.newline = '\n'
+
+        # Establish config file and database locations
         try:
             self.data_dir = Path(os.environ["XDG_DATA_HOME"]).expanduser().resolve()
         except KeyError:
@@ -45,23 +58,42 @@ class BakConfiguration(dict):
             _cfg = Config(str(self.config_file))
 
         self.cfg = _cfg
+
         super().__init__()
 
     def __getitem__(self, item):
-        return self.cfg[item]
+        if item in self.SETTABLE_VALUES:
+            item = self.SETTABLE_VALUES[item]
+        return translate_config_value(self.cfg[item])
+
+    def get(self, item, literal=True):
+        """ 
+        "Override" dict.get() in specific cases: use this to get the actual value of complex options,
+        rather than whatever bak parses it into (like the actual command for `bak diff`, rather than
+        an array of commands and arguments)
+        
+        Called by `bak config <value>`, just for good measure
+        """
+        if literal:
+        # `and` would work, but why make the second check if this is a regular dict.get()?
+            if item in self.SETTABLE_VALUES:
+                return self.cfg[self.SETTABLE_VALUES[item]]
+        return super().get(item)
 
     def __setitem__(self, item: str, value: str):
+        if item in self.SETTABLE_VALUES:
+            item = self.SETTABLE_VALUES[item]
         if item not in self.DEFAULT_VALUES:
             err = f"{item} is not a valid bak setting. Valid settings include:\n"
             for setting in self.DEFAULT_VALUES:
-                err += f"\n\t\t\t{setting}"
+                err += f"\n\t\t\t{setting + self.newline}"
             raise KeyNotFoundError(err)
         else:
             if str(value).lower not in ('true', 'false'):
                 if value is None:
                     value = 'null'
                 else:
-                    value = f"'{value}'"
+                    value = f"{value}"
 
             _config = None
             with open(self.config_file, 'r') as _file:
@@ -70,5 +102,15 @@ class BakConfiguration(dict):
             with open(self.config_file, 'w') as _file:
                 _file.write(_config)
             self.cfg = Config(str(self.config_file))
+
+EQUIVALENT_VALUES = {'false': False,
+                     'true': True,
+                     'null': None}
+def translate_config_value(val):
+    if val in EQUIVALENT_VALUES.values():
+        return val
+    if val not in EQUIVALENT_VALUES:
+        return val
+    return EQUIVALENT_VALUES[val]
 
 bak_cfg = BakConfiguration()
